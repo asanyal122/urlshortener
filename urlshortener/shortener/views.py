ca75e4aa710826e181599f1e5bd318d7e72c2d57
asanyal122 <asanyal122@gmail.com>
2022-06-08 12:04:25 +0530

    Small fix added for duplicate url entries

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -7,13 +7,25 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'index.html')
 
+def get_existing(url):
+    existing_url=None
+    try:
+        existing_url=Url.objects.get(link=url)
+    except Url.DoesNotExist:
+        existing_url=None
+
+    return existing_url
+
 def create(request):
     if request.method=='POST':
         url=request.POST['link']
         uid=str(uuid.uuid4())[:5]
-        new_url=Url(link=url,uid=uid)
-        new_url.save() #duplicate entries
-        return HttpResponse(uid)
+        new_url=get_existing(url)
+        
+        if(new_url is None):
+            new_url=Url(link=url,uid=uid)
+            new_url.save()
+        return HttpResponse(new_url.uid)
 
 def go(request,pk):
     url=Url.objects.get(uid=pk)
